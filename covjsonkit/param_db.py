4f28f09464f2d6e7e61be8e671aeffda2b1557af
awarde96 <awarde@tcd.ie>
2024-08-13 13:38:57 +0000

    Users can now provide their own params and param ids

--- covjsonkit/param_db.py
+++ covjsonkit/param_db.py
@@ -2,11 +2,13 @@ import json
 import os
 from os.path import dirname
 
+#param_dir = os.environ['PARAM_DIR']
+param_dir = os.getenv('PARAM_DIR', 'ecmwf')
 
 def get_param_from_db(param_id):
     """
     import requests
-    url = f"https://codes.ecmwf.int/parameter-database/api/v1/param/?format=json&searcg={param_id}"
+    url = f"https://codes.ecmwf.int/parameter-database/api/v1/param/?format=json&search={param_id}"
     response = requests.get(url)
     if response.status_code == 200:
         for param in response.json():
@@ -20,42 +22,42 @@ def get_param_from_db(param_id):
     except BaseException:
         param_id = get_param_id_from_db(param_id)
 
-    param_path = os.path.join(dirname(__file__), "data/param.json")
+    param_path = os.path.join(dirname(__file__), f"data/{param_dir}/param.json")
     with open(param_path) as f:
         params = json.load(f)
     return params[str(param_id)]
 
 
 def get_param_id_from_db(param_id):
-    param_id_path = os.path.join(dirname(__file__), "data/param_id.json")
+    param_id_path = os.path.join(dirname(__file__), f"data/{param_dir}/param_id.json")
     with open(param_id_path) as f:
         param_ids = json.load(f)
     return param_ids[str(param_id)]
 
 
 def get_unit_from_db(unit_id):
-    unit_path = os.path.join(dirname(__file__), "data/unit.json")
+    unit_path = os.path.join(dirname(__file__), f"data/{param_dir}/unit.json")
     with open(unit_path) as f:
         units = json.load(f)
     return units[str(unit_id)]
 
 
 def get_param_ids():
-    param_id_path = os.path.join(dirname(__file__), "data/param_id.json")
+    param_id_path = os.path.join(dirname(__file__), f"data/{param_dir}/param_id.json")
     with open(param_id_path) as f:
         param_ids = json.load(f)
     return param_ids
 
 
 def get_params():
-    param_path = os.path.join(dirname(__file__), "data/param.json")
+    param_path = os.path.join(dirname(__file__), f"data/{param_dir}/param.json")
     with open(param_path) as f:
         params = json.load(f)
     return params
 
 
 def get_units():
-    unit_path = os.path.join(dirname(__file__), "data/unit.json")
+    unit_path = os.path.join(dirname(__file__), f"data/{param_dir}/unit.json")
     with open(unit_path) as f:
         units = json.load(f)
     return units
