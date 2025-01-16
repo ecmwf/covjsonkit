b40360da6a2fd7cfee575148f67fb90954248ab0
awarde96 <awarde@tcd.ie>
2025-01-16 13:39:17 +0000

    Fix fro climate-dt

--- covjsonkit/encoder/VerticalProfile.py
+++ covjsonkit/encoder/VerticalProfile.py
@@ -93,7 +93,7 @@ class VerticalProfile(Encoder):
         fields["lat"] = 0
         fields["param"] = 0
         fields["number"] = [0]
-        fields["step"] = 0
+        fields["step"] = [0]
         fields["dates"] = []
         fields["levels"] = [0]
 
