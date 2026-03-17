287222ed398abc4bbce04d54027124a63b33e66a
awarde96 <adamjwarde@gmail.com>
2026-03-17 10:50:02 +0000

    Fix ruff

--- covjsonkit/__init__.py
+++ covjsonkit/__init__.py
@@ -1,7 +1,1 @@
-import covjsonkit.api
-import covjsonkit.decoder.TimeSeries
-import covjsonkit.decoder.VerticalProfile
-import covjsonkit.encoder.TimeSeries
-import covjsonkit.encoder.VerticalProfile
-
 from .version import __version__
